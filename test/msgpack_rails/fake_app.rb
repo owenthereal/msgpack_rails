66a1403fa9057d14f7fb096c86455f0afe5f5474
Masato Ikeda <masato.ikeda@gmail.com>
2018-05-04 20:22:08 +0900

    Add rendering test

---
+++ test/msgpack_rails/fake_app.rb
@@ -0,0 +1,15 @@
+require "action_controller/railtie"
+require "active_model/railtie"
+
+class FakeApp < Rails::Application
+  config.secret_token = config.secret_key_base = "1234567890"
+  config.session_store :cookie_store, key: '_myapp_session'
+  config.active_support.deprecation = :log
+  config.eager_load = false
+  config.root = File.dirname(__FILE__)
+end
+Rails.backtrace_cleaner.remove_silencers!
+Rails.application.initialize!
+
+class ApplicationController < ActionController::Base
+end
