8a56215424cee668a3e0ede884c37171e192133d
Masato Ikeda <masato.ikeda@gmail.com>
2018-05-04 21:50:07 +0900

    Support Integer unification for CRuby 2.4+
    Rails 4.2.8+ supports this, so I dropped testing Rails 4.1 against CRuby
    2.4 and 2.5.

--- lib/msgpack_rails/activesupport/core_ext/object/to_msgpack.rb
+++ lib/msgpack_rails/activesupport/core_ext/object/to_msgpack.rb
@@ -8,7 +8,13 @@ end
 # The msgpack gem adds a few modules to Ruby core classes containing :to_msgpack definition, overwriting
 # their default behavior. That said, we need to define the basic to_msgpack method in all of them,
 # otherwise they will always use to_msgpack gem implementation.
-[Object, NilClass, TrueClass, FalseClass, Fixnum, Bignum, Float, String, Array, Hash, Symbol].each do |klass|
+klasses = [Object, NilClass, TrueClass, FalseClass, Float, String, Array, Hash, Symbol]
+if 1.class.name == "Integer"
+  klasses.push(Integer)
+else
+  klasses.push(Fixnum, Bignum)
+end
+klasses.each do |klass|
   klass.class_eval do
     alias_method :msgpack_to_msgpack, :to_msgpack if respond_to?(:to_msgpack)
     # Dumps object in msgpack. See http://msgpack.org for more infoa
